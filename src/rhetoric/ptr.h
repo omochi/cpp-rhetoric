a649d32df7e52a3f7282d1b5e58135b455799db3
omochi <omochi.metaru@gmail.com>
2017-06-22 02:27:53 +0900

    NEW

--- src/rhetoric/ptr.h
+++ src/rhetoric/ptr.h
@@ -2,6 +2,8 @@
 
 #include "./std_dependency.h"
 
+#define RHETORIC_NEW(type, ...) (std::shared_ptr<type>(new type(__VA_ARGS__)))
+
 namespace rhetoric {
     template <typename T> using Ptr = std::shared_ptr<T>;
     template <typename T> using WeakPtr = std::weak_ptr<T>;
--- src/rhetoric/ptr_inline.h
+++ src/rhetoric/ptr_inline.h
@@ -4,6 +4,7 @@ namespace rhetoric {
     template <typename T, typename ...Args>
     Ptr<T> New(Args&&... args)
     {
-        return std::make_shared<T>(std::forward<Args>(args)...);
+        T * instance = new T(std::forward<Args>(args)...);
+        return std::shared_ptr<T>(instance);
     }
 }
