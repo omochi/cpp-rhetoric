fbdd795fac22cd746b5f1e42bd002c7250f168ab
omochi <omochi.metaru@gmail.com>
2017-07-14 19:25:48 +0900

    shared from this

--- src/rhetoric/shared_from_this_macro.h
+++ src/rhetoric/shared_from_this_macro.h
@@ -2,9 +2,9 @@
 
 #define RHETORIC_SUBCLASS_SHARED_FROM_THIS(this_type, super_type) \
 rhetoric::Ptr<const this_type> shared_from_this() const { \
-    return std::static_pointer_cast<const this_type>(super_type::shared_from_this()); \
+    return std::static_pointer_cast<const this_type>(this->super_type::shared_from_this()); \
 } \
 rhetoric::Ptr<this_type> shared_from_this() { \
-    return std::static_pointer_cast<this_type>(super_type::shared_from_this()); \
+    return std::static_pointer_cast<this_type>(this->super_type::shared_from_this()); \
 }
 
